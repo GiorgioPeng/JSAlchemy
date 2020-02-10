bc9332434f165c15be01f72cab14926e46bdcd81
giorgiopeng <84867579@qq.com>
2020-02-10 21:25:09 +0800

    xor多层神经网络

--- logistic-regression/creatData.js
+++ logistic-regression/creatData.js
@@ -1,24 +1,25 @@
 export function getData(numSamples) {
+  //numSamples 生成点的数量（蓝色点和黄色点加起来） 
   let points = [];
   function genGauss(cx, cy, label) {
     for (let i = 0; i < numSamples / 2; i++) {
-      let x = normalRandom(cx);
-      let y = normalRandom(cy);
-      points.push({ x, y, label });
+      let x = normalRandom(cx);//x 以 cx 为中心的正太分布
+      let y = normalRandom(cy);//y 以 cy 为中心的正太分布
+      points.push({ x, y, label }); //两者组合或者一个二维正太分布
     }
   }
-  genGauss(2, 2, 1);
-  genGauss(-2, -2, 0);
+  genGauss(2, 2, 1);//生成蓝色的点 
+  genGauss(-2, -2, 0);//生成黄色的点
   return points;
 }
-
+// Box-Muller 算法  https://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform
 function normalRandom(mean = 0, variance = 1) {
   let v1, v2, s;
   do {
     v1 = 2 * Math.random() - 1;
     v2 = 2 * Math.random() - 1;
     s = v1 * v1 + v2 * v2;
-  } while (s > 1); //s<1的话说明=> v1和v2都小于0.5
+  } while (s > 1); //s<1来确定s在圆圈内
   let result = Math.sqrt((-2 * Math.log(s)) / s) * v1;
   return mean + Math.sqrt(variance) * result;
 }
---
+++ xor/script.js
@@ -0,0 +1,47 @@
+import * as tfvis from '@tensorflow/tfjs-vis'
+import * as tfjs from '@tensorflow/tfjs'
+import { getData } from './data'
+import { relu } from '@tensorflow/tfjs';
+window.onload = async () => {
+    const points = getData(400);
+    tfvis.render.scatterplot(
+        { name: 'xor' },
+        {
+            values: [points.filter(point => point.label === 1), points.filter(point => point.label === 0)]
+        }
+    )
+    const model = tfjs.sequential();
+    model.add(tfjs.layers.dense({
+        units: 4,
+        inputShape: [2],
+        activation: 'relu'
+    })); //设置隐藏层，只有第一层需要设置inputshape，如果把隐藏层中的激活函数去掉，则这个隐藏层没有任何作用（线性组合还是线性，无法解决非线性问题）
+    model.add(tfjs.layers.dense({
+        units: 1, activation: 'sigmoid'
+    }));//设置输出层
+    model.compile({ loss: tfjs.losses.logLoss, optimizer: tfjs.train.adam(0.1) });//补充损失函数和优化器,logloss常用于逻辑损失函数
+    const inputs = tfjs.tensor(points.map(point => [point.x, point.y]));
+    const labels = tfjs.tensor(points.map(point => point.label));
+    await model.fit(inputs, labels, {
+        batchSize: 40,
+        epochs: 20,
+        callbacks: tfvis.show.fitCallbacks(
+            {
+                name: '训练过程'
+            },
+            ['loss']
+        )
+    })
+    window.predict = async (form) => {
+        const pred = await model.predict(
+            tfjs.tensor(
+                [
+                    [
+                        form.x.value * 1, form.y.value * 1//乘1将字符串转化乘数字
+                    ]
+                ]
+            )
+        )
+        alert(`预测结果：${pred.dataSync()[0]}`)
+    }
+}
